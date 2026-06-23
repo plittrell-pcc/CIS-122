658fdea5ba75f1d1f27b39b6b06e0827ffeb1b38
Paul Littrell <paul.littrell@pcc.edu>
2026-06-22 23:41:40 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#***************************************************************
+#
+# Author: Paul Littrell
+# Lab:    CIS-122 Lab 1
+# Date:   June 22,2026
+# Desc:   This is my "Hello World" program
+# Input:  No user input
+# Output: Prints "Hello World!" to the screen
+# Source: Lab 1 specifications
+#
+#***************************************************************
+
+# Print Statement
+
+print("Hello World!")
