59d3cdb683bbe91caa5386f3325dd9dbc3361f65
Paul Littrell <paul.littrell@pcc.edu>
2026-06-29 19:18:20 -0700

    Created vocab.py practice program.

---
+++ vocab.py
@@ -0,0 +1,67 @@
+#***************************************************************
+#
+# Author: Paul Littrell
+# Lab:    CIS-122 Practice Problem
+# Date:   June 29,2026
+# Desc:   This program calculates calories from eating cookies
+# Input:  The number of cookies eaten
+# Output: Total calories of the cookies eaten
+# Source: Unit 2 Python Practice
+#         Translate the pseudocode below to Python
+# GitHub: https://github.com/plittrell-pcc/CIS-122.git
+#
+#***************************************************************
+
+# Display "Hello World!"
+print("Hello World!\n")
+
+# Declare variable course and set to "CIS 122"
+course = "CIS 122"
+
+# Declare variable name and set to "Intro to Software Design"
+name = "Intro to Software Design"
+
+# Display "Welcome to", course, ":", name
+print("Welcome to", course, ":", name, "\n")
+
+# Declare float variable a and set to 1.1
+a = 1.1
+
+# Declare integer variable b and set to 3
+b = 3
+
+# Declare float variable c and set to 0.0
+c = 0.0
+
+# Calculate sum of a + b and store into c
+c = a + b
+
+# Display "The sum of", a, "and", b, "is", c
+print("The sum of", a, "and", b, "is", c, "\n")
+
+# Declare string variable language and set to "Python"
+language = "Python"
+
+# Declare string variable adjective and set to "Fun"
+adjective = "Fun"
+
+# Display "Learning", language, "is", adjective
+print("Learning", language, "is", adjective, "\n")
+
+# Declare pizzas = 10
+pizzas = 10
+
+# Declare slices_per_pizza = 8
+slices_per_pizza = 8
+
+# Declare total_slices = 0
+total_slices = 0
+
+# Calculate total_slices = pizzas * slices_per_pizza
+total_slices = pizzas * slices_per_pizza
+
+# Display "There are", total_slices, "slices in", pizzas, "pizzas."
+print("There are", total_slices, "slices in", pizzas, "pizzas.")
+
+# Display total_slices, ": It must be dinner time!"
+print(total_slices, ": It must be dinner time!\n")
